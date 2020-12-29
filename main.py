b55788471ca2c2e1cc6b422428166b9f4e6633ce
smazydev <alizafar1437@gmail.com>
2020-12-30 04:00:14 +0500

    Completed Base UI

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from tkinter import *
-
-#-------------------------UI SETUP-------------------------#
-
+#-------------------------CONSTANTS-------------------------#
+BACKGROUND_COLOR = "#B1DDC6"
 
 
 
@@ -20,9 +19,23 @@ from tkinter import *
 
 window = Tk()
 window.title("Flashcard Language Learning Application")
-window.geometry("700x500")
+window.config(padx=50,pady=50,bg=BACKGROUND_COLOR)
 window.resizable(False,False)
 
+canvas = Canvas(width=800,height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400,263,image=card_front_img)
+canvas.create_text(400,150,text="Title",font=("Ariel",40,"italic"))
+canvas.create_text(400,263,text="word",font=("Ariel",60,"bold"))
+canvas.config(bg=BACKGROUND_COLOR,highlightthickness=0)
+canvas.grid(row=0,column=0,columnspan=2)
 
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image,highlightthickness=0)
+unknown_button.grid(row=1,column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image,highlightthickness=0)
+known_button.grid(row=1,column=1)
 
 window.mainloop()
