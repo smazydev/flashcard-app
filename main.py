8cff4ddd0702941a47153e836cb23fe9189e9cb4
smazydev <alizafar1437@gmail.com>
2020-12-30 03:32:03 +0500

    Added App name,made window not resizable

---
+++ main.py
@@ -0,0 +1,28 @@
+from tkinter import *
+
+#-------------------------UI SETUP-------------------------#
+
+
+
+
+
+#-------------------------UI SETUP-------------------------#
+
+
+
+
+#-------------------------UI SETUP-------------------------#
+
+
+
+
+#-------------------------UI SETUP-------------------------#
+
+window = Tk()
+window.title("Flashcard Language Learning Application")
+window.geometry("700x500")
+window.resizable(False,False)
+
+
+
+window.mainloop()
